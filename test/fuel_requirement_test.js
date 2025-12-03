02ea96bf7c03afcb4f2346c119698680ebdadb81
Dilliswararao Galala <dilliswararaogalala@Dilliswararaos-MacBook-Pro.local>
2025-12-03 10:17:35 +0530

    passed one test case

---
+++ test/fuel_requirement_test.js
@@ -0,0 +1,14 @@
+import { assertEquals } from "jsr:@std/assert";
+import { fuelRequires } from "../src/fuel_requrierment.js";
+
+Deno.test("small module", () => {
+  assertEquals(fuelRequires("123"), 39);
+});
+
+Deno.test("small module", () => {
+  assertEquals(fuelRequires("123"), 39);
+});
+
+Deno.test("small module", () => {
+  assertEquals(fuelRequires("123"), 39);
+});
