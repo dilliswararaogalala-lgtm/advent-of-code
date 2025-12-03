598a6c8b0a387c9b46deac79297309270345bd86
Dilliswararao Galala <dilliswararaogalala@Dilliswararaos-MacBook-Pro.local>
2025-12-03 14:34:51 +0530

    solved day2 problem

--- day2/opcode.js
+++ day2/opcode.js
@@ -1,14 +1,15 @@
 const input = Deno.readTextFileSync("opcode.txt");
-const code = input;
+const opcode = input;
 
-const opcodes = code.split(/,/).map(x => parseInt(x));
-opcodes[1] = 12
-opcodes[2] = 2
+const code = opcode.split(/,/).map((x) => parseInt(x));
 
 const mul = (x, y) => x * y;
 const add = (x, y) => x + y;
 
-const programAlaram = (code) => {
+const intcodeProgram = (code, i, j) => {
+  code[1] = i;
+  code[2] = j;
+
   const performOperation = (operation) => (code, i) => {
     const value1 = code[code[i + 1]];
     const value2 = code[code[i + 2]];
@@ -27,14 +28,25 @@ const programAlaram = (code) => {
     99: halt,
   };
 
-  for (let i = 0; i < code.length; i++) {
-    if (code[i] in operations) {
-      i = operations[code[i]](code, i);
+  for (let index = 0; index < code.length; index++) {
+    if (code[index] in operations) {
+      index = operations[code[index]](code, index);
     }
   }
 
-  return code
+  return code[0];
 };
 
-console.log(programAlaram(opcodes)[0]);
+const findNounAndVerb = (code, values) => {
+  for (let i = 1; i <= 100; i++) {
+    for (let j = 1; j <= 100; j++) {
+      if (intcodeProgram([...code], i, j) === 19690720) {
+        values.noun = i;
+        values.verb = j;
+      }
+    }
+  }
+  return ((100 * values.noun) + values.verb);
+};
 
+console.log(findNounAndVerb([...code], { noun: 0, verb: 0 }));
