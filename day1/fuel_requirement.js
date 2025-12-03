37dcd4c9fd94466d38e59af3a979f59e8d5d173b
Dilliswararao Galala <dilliswararaogalala@Dilliswararaos-MacBook-Pro.local>
2025-12-03 11:42:33 +0530

    completed day one puzzle

--- day1/fuel_requirement.js
+++ day1/fuel_requirement.js
@@ -1,9 +1,28 @@
 const modules = Deno.readTextFileSync("modules.txt").split(/\n/);
 
+const dbg = (x) => {
+  console.log(x);
+  return x;
+};
+
 const fuelRequirement = (spaceCraft) => Math.floor(spaceCraft / 3) - 2;
 
-console.log(
+const fuelForFuels = (mass) => {
+  const newMass = fuelRequirement(mass);
+  if (newMass <= 0) {
+    return 0;
+  }
+  return newMass + fuelForFuels(newMass);
+};
+
+dbg(
   modules
     .map((x) => fuelRequirement(x))
-    .reduce((totalFuel, currentFuel) => totalFuel + currentFuel),
+    .reduce((totalFuel, currentFuel) => totalFuel + currentFuel, 0),
+);
+
+dbg(
+  modules
+    .map((x) => fuelForFuels(x))
+    .reduce((totalFuel, currentFuel) => totalFuel + currentFuel, 0),
 );
