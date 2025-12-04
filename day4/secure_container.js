f1efd575434878aa207009c4a7560aa5d540abe8
Dilliswararao Galala <dilliswararaogalala@Dilliswararaos-MacBook-Pro.local>
2025-12-04 10:11:29 +0530

    solved day4 puzzle part 1

--- day4/secure_container.js
+++ day4/secure_container.js
@@ -1,17 +1,34 @@
-const input = "123789";
-
-const inputInArray = input.split("");
+const puzzleInput = "146810-612564";
 
 const dbg = (x) => {
   console.log(x);
   return x;
-}
+};
+
+const isInOrder = (input) => {
+  return input.split("").sort().join("") === input;
+};
 
 const checkPassword = (input) => {
-  for (let index = 1; index < input.length; index++) {
-    if (input[index - 1] === input[index]) return true;
+  if (isInOrder(input)) {
+    for (let index = 1; index < input.length; index++) {
+      if (input[index - 1] === input[index]) {
+        return true;
+      }
+    }
   }
   return false;
 };
 
-dbg(checkPassword(input));
+const countPossiblePasswords = (puzzleInput) => {
+  const startValue = puzzleInput[0];
+  const endvalue = puzzleInput[1];
+  let count = 0;
+  for(let i = startValue; i <= endvalue; i++){
+    if(checkPassword(i.toString())) count++;
+  }
+
+  return count;
+}
+
+dbg(countPossiblePasswords(puzzleInput.split(/-/).map(x => +x)));
