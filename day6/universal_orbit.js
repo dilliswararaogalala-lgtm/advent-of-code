1b73269064d7bc3732849de1d1d2393ae53c932e
Dilliswararao Galala <dilliswararaogalala@Dilliswararaos-MacBook-Pro.local>
2025-12-05 18:04:18 +0530

    solved day6 puzzle

--- day6/universal_orbit.js
+++ day6/universal_orbit.js
@@ -1,22 +1,40 @@
 const orbits = Deno.readTextFileSync("universal_orbit_map.txt");
+//const orbits = "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L\nK)YOU\nI)SAN"
 
 const dbg = (x) => {
   console.log(x);
   return x
 }
+
 const getOrbits = (orbit, currentOrbit) => {
   orbit[currentOrbit[1]] = currentOrbit[0];
   return orbit;
 }
 
-const directOrbits = (orbits.split(/\n/).map(x => x.split(/\)/)).reduce(getOrbits, {}));
-let count = 0;
+const directOrbits = (orbits
+  .split(/\n/)
+  .map(x => x.split(/\)/))
+  .reduce(getOrbits, {}));
 
-for (let key in directOrbits) {
-  while(directOrbits[key] !== undefined){
-    count++;
-    key = directOrbits[key];
+const getPath = (directOrbits, pointer) => {
+  const path = [];
+  while(directOrbits[pointer] !== undefined){
+    pointer = directOrbits[pointer];
+    path.push(pointer);
+  }
+  return path;
+}
+
+const pathOfYou = dbg(getPath(directOrbits, "YOU"))
+const pathOfSan = dbg(getPath(directOrbits, "SAN"))
+
+const getTheCommonPath = (pathOfYou, pathOfSan) => {
+  for(let i = 0; i < pathOfYou.length; i++){
+    if(pathOfSan.includes(pathOfYou[i])){
+      return [i, pathOfSan.indexOf(pathOfYou[i])];
+    }
   }
 }
 
-dbg(count);
+const [p1,p2] = getTheCommonPath(pathOfYou, pathOfSan);
+console.log(p1+p2);
