fcd69c48e45ae9b330fe87ce65b215b0185671c3
Dilliswararao Galala <dilliswararaogalala@Dilliswararaos-MacBook-Pro.local>
2025-12-05 17:04:18 +0530

    solved part 1 of day6

--- day6/universal_orbit.js
+++ day6/universal_orbit.js
@@ -1,15 +1,22 @@
-const orbits = "COM)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L";
+const orbits = Deno.readTextFileSync("universal_orbit_map.txt");
 
-const directOrbits = orbits
-  .split(/\n/)
-  .map((x) => x.split(/\)/));
+const dbg = (x) => {
+  console.log(x);
+  return x
+}
+const getOrbits = (orbit, currentOrbit) => {
+  orbit[currentOrbit[1]] = currentOrbit[0];
+  return orbit;
+}
 
-const uniqueOrbits = directOrbits
-  .flatMap((x) => x)
-  .reduce((uniqueOrbits, orbit) => {
-    if (!uniqueOrbits.includes(orbit)) uniqueOrbits.push(orbit);
-    return uniqueOrbits;
-  }, [])
-  .sort((a, b) => a - b);
+const directOrbits = (orbits.split(/\n/).map(x => x.split(/\)/)).reduce(getOrbits, {}));
+let count = 0;
 
-console.log(uniqueOrbits);
+for (let key in directOrbits) {
+  while(directOrbits[key] !== undefined){
+    count++;
+    key = directOrbits[key];
+  }
+}
+
+dbg(count);
