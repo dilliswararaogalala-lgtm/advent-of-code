c1ae48d46ee88c368c5a99c419e73eef09654425
Dilliswararao Galala <dilliswararaogalala@Dilliswararaos-MacBook-Pro.local>
2025-12-07 13:02:03 +0530

    solved first part of day8

--- day8/space_image_format.js
+++ day8/space_image_format.js
@@ -1,9 +1,30 @@
 import { chunk } from "jsr:@std/collections";
 
-const imageData = "123456789012".split('');
+const imageData = Deno.readTextFileSync("space_image_format.txt").split("");
 
-const imageSize = 3 * 2;
+const imageSize = 25 * 6;
 
 const chunks = chunk(imageData, imageSize);
 
-console.log(chunks);
+
+const occurencesOfAll = (frequencies, layer) => {
+  const ocuureces = (frequencyTable, number) => {
+    frequencyTable[number] = frequencyTable[number] || 0;
+    frequencyTable[number] += 1;
+    return frequencyTable;
+  };
+
+  frequencies.push(layer.reduce(ocuureces, {}));
+  return frequencies;
+};
+
+const allFrequencies = chunks.reduce(occurencesOfAll, []);
+
+const lowestZeroes = allFrequencies
+  .filter((object) => object[0])
+  .reduce((min, current) => {
+    return current[0] < min[0] ? current : min;
+  });
+
+console.log(lowestZeroes[1] * lowestZeroes[2]);
+
